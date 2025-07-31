aeafb425e942bd2caf30e5e799c7a73583951b38
John Crickett <3667898+JohnCrickett@users.noreply.github.com>
2025-07-31 14:54:09 +0100

    Step 2

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Rust solution to Coding Challenges [build your own curl](https://codingchallenges.fyi/challenges/challenge-curl/)
 
 ## Testing
-As this is a complete solution the intermediate build steps aren't shown.
+As this is a complete solution, the intermediate build steps aren't shown.
 
 ## Get
 ```bash
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use std::net::TcpStream;
+use std::io::{Write, Read};
+use std::error::Error;
+use std::time::Duration;
+
 use clap::Parser;
-use url::{Url, ParseError};
+use url::{Url};
 
 #[derive(Parser, Debug)]
 #[command(author = "John Crickett", version, about="rscurl, a simple curl clone in Rust")]
@@ -30,4 +35,35 @@ fn main() {
     println!("Host: {}", parsed_url.host_str().unwrap());
     println!("Accept: */*");
 
+    match send_request(parsed_url.host_str().unwrap(), 80, parsed_url.path()) {
+        Ok(response) => {
+            println!("Received response:");
+            println!("{}", response);
+        },
+        Err(e) => eprintln!("Error: {}", e),
+    }
+}
+
+fn send_request(host: &str, port: u16, path: &str) -> Result<String, Box<dyn Error>> {
+    let mut stream = TcpStream::connect((host, port))?;
+
+    stream.set_read_timeout(Some(Duration::from_secs(30)))?;
+    stream.set_write_timeout(Some(Duration::from_secs(30)))?;
+
+    let request = format!(
+        "GET {} HTTP/1.1\r\n\
+         Host: {}\r\n\
+         Connection: close\r\n\
+         User-Agent: rust-client\r\n\
+         Accept: */*\r\n\
+         \r\n",
+        path, host
+    );
+
+    stream.write_all(request.as_bytes())?;
+
+    let mut response = String::new();
+    stream.read_to_string(&mut response)?;
+
+    Ok(response)
 }
