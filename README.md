fb5a81fe9a6d48075c209d94a7d837271b4f4cf1
John Crickett <3667898+JohnCrickett@users.noreply.github.com>
2025-10-23 22:48:28 +0100

    Step 5 - Post

--- README.md
+++ README.md
@@ -4,9 +4,118 @@ Rust solution to Coding Challenges [build your own curl](https://codingchallenge
 ## Testing
 As this is a complete solution, the intermediate build steps aren't shown.
 
-## Get
+
+### Get
 ```bash
-./target/release/rscurl http://eu.httpbin.org:80/get
+cargo run -- http://eu.httpbin.org:80/get
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.12s
+     Running `target/debug/rscurl 'http://eu.httpbin.org:80/get'`
+{
+  "args": {}, 
+  "headers": {
+    "Accept": "*/*", 
+    "Host": "eu.httpbin.org", 
+    "User-Agent": "rust-client", 
+    "X-Amzn-Trace-Id": "Root=1-68f94415-6863c0f543a4a60d4d43783d"
+  }, 
+  "origin": "84.69.23.61", 
+  "url": "http://eu.httpbin.org/get"
+}
 
 ```
+
+### Get With Verbose
+### Get
+```bash
+cargo run --  -v  http://eu.httpbin.org:80/get
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.10s
+     Running `target/debug/rscurl -v 'http://eu.httpbin.org:80/get'`
+> GET /get HTTP/1.1
+> Host: eu.httpbin.org
+> Accept: */*
+>
+< HTTP/1.1 200 OK
+< Date: Wed, 22 Oct 2025 20:53:29 GMT
+< Content-Type: application/json
+< Content-Length: 258
+< Connection: close
+< Server: gunicorn/19.9.0
+< Access-Control-Allow-Origin: *
+< Access-Control-Allow-Credentials: true
+<
+{
+  "args": {}, 
+  "headers": {
+    "Accept": "*/*", 
+    "Host": "eu.httpbin.org", 
+    "User-Agent": "rust-client", 
+    "X-Amzn-Trace-Id": "Root=1-68f94448-70fc5a6865883f047a17dea1"
+  }, 
+  "origin": "84.69.23.61", 
+  "url": "http://eu.httpbin.org/get"
+}
+```
+
+### Delete
+```bash
+cargo run -- -X DELETE http://eu.httpbin.org/delete
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.10s
+     Running `target/debug/rscurl -X DELETE 'http://eu.httpbin.org/delete'`
+{
+  "args": {}, 
+  "data": "", 
+  "files": {}, 
+  "form": {}, 
+  "headers": {
+    "Accept": "*/*", 
+    "Host": "eu.httpbin.org", 
+    "User-Agent": "rust-client", 
+    "X-Amzn-Trace-Id": "Root=1-68f9446d-6c75263e733dcc3c5ea70f9d"
+  }, 
+  "json": null, 
+  "origin": "84.69.23.61", 
+  "url": "http://eu.httpbin.org/delete"
+}
+
+
+```
+### POST
+```bash
+cargo run -- -X POST -d '{"key": "value"}' -H "Content-Type: application/json" http://eu.httpbin.org/post
+% cargo run -- -X POST -d '{"key": "value"}' -H "Content-Type: application/json" http://eu.httpbin.org/post 
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.11s
+     Running `target/debug/rscurl -X POST -d '{"key": "value"}' -H 'Content-Type: application/json' 'http://eu.httpbin.org/post'`
+connecting to: http://eu.httpbin.org/post
+{
+  "args": {}, 
+  "data": "{\"key\": \"value\"}", 
+  "files": {}, 
+  "form": {}, 
+  "headers": {
+    "Accept": "*/*", 
+    "Content-Length": "16", 
+    "Content-Type": "application/json", 
+    "Host": "eu.httpbin.org", 
+    "User-Agent": "rscurl", 
+    "X-Amzn-Trace-Id": "Root=1-68faa1f0-48ed23dd27c14cd60aa19cc0"
+  }, 
+  "json": {
+    "key": "value"
+  }, 
+  "origin": "84.69.23.61", 
+  "url": "http://eu.httpbin.org/post"
+}
+```
+
+
+### PUT
+```bash
+cargo run -- -X PUT -d '{"key2": "value2"}' -H "Content-Type: application/json" http://eu.httpbin.org/put
+
+```
+
+
+
+
+
 
--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,16 @@ struct Arguments {
     /// Enable verbose mode
     #[arg(short = 'v', long = "verbose")]
     verbose: bool,
+    /// HTTP request body (adds Content-Length and sends a body)
+    #[arg(short = 'd', long = "data")]
+    data: Option<String>,
+    /// Add header (can be used multiple times)
+    #[arg(short = 'H', long = "header")]
+    headers: Vec<String>,
 }
 
 fn main() {
     let args = Arguments::parse();
-    println!("connecting to: {}", args.url);
 
     let parsed_url = match Url::parse(&args.url) {
         Ok(url) => url,
@@ -35,13 +40,48 @@ fn main() {
         }
     };
 
+    let host = match parsed_url.host_str() {
+        Some(h) => h,
+        None => {
+            eprintln!("Error: URL must include a host");
+            std::process::exit(1);
+        }
+    };
+
+    let port = parsed_url.port_or_known_default().unwrap_or(80);
+
+
+    let user_headers: Vec<String> = args
+        .headers
+        .iter()
+        .map(|h| h.trim().to_string())
+        .filter(|h| !h.to_lowercase().starts_with("content-length:"))
+        .collect();
+
+    let has_content_type = user_headers
+        .iter()
+        .any(|h| h.to_lowercase().starts_with("content-type:"));
+
+
     if args.verbose {
         println!("> {} {} HTTP/1.1", args.method, parsed_url.path());
         println!("> Host: {}", parsed_url.host_str().unwrap());
-        println!("> Accept: */*\n>");
+        println!("> Accept: */*");
+        for h in &user_headers {
+            println!("> {}", h);
+        }
+        if let Some(ref body) = args.data {
+            let len = body.as_bytes().len();
+            if !has_content_type {
+                println!("> Content-Type: application/x-www-form-urlencoded");
+            }
+            println!("> Content-Length: {}", len);
+        }
+        println!(">");
+
     }
 
-    match send_request(parsed_url.host_str().unwrap(), 80, parsed_url.path(), &args.method) {
+    match send_request(host, port, parsed_url.path(), &args.method, args.data.as_deref(), &args.headers) {
         Ok(response) => {
             let parts: Vec<&str> = response.split("\r\n\r\n").collect();
 
@@ -62,21 +102,45 @@ fn main() {
     }
 }
 
-fn send_request(host: &str, port: u16, path: &str, method: &str) -> Result<String, Box<dyn Error>> {
+fn send_request(host: &str, port: u16, path: &str, method: &str, body: Option<&str>, headers: &[String]) -> Result<String, Box<dyn Error>> {
     let mut stream = TcpStream::connect((host, port))?;
 
     stream.set_read_timeout(Some(Duration::from_secs(30)))?;
     stream.set_write_timeout(Some(Duration::from_secs(30)))?;
 
-    let request = format!(
+    let mut request = format!(
         "{} {} HTTP/1.1\r\n\
          Host: {}\r\n\
          Connection: close\r\n\
-         User-Agent: rust-client\r\n\
-         Accept: */*\r\n\
-         \r\n",
+         User-Agent: rscurl\r\n\
+         Accept: */*\r\n",
         method, path, host
     );
+
+    for header in headers {
+        request.push_str(header);
+        request.push_str("\r\n");
+    }
+
+    let has_content_type = headers
+        .iter()
+        .any(|header| header.to_lowercase().starts_with("content-type:"));
+
+    if let Some(b) = body {
+        // Add a default Content-Type if the user didn't provide one
+        if !has_content_type {
+            request.push_str("Content-Type: application/x-www-form-urlencoded\r\n");
+        }
+        // Always send correct Content-Length for provided body
+        request.push_str(&format!("Content-Length: {}\r\n", b.as_bytes().len()));
+    }
+    request.push_str("\r\n");
+
+    if let Some(b) = body {
+        request.push_str(b);
+    }
+
+    request.push_str("\r\n");
 
     stream.write_all(request.as_bytes())?;
 
